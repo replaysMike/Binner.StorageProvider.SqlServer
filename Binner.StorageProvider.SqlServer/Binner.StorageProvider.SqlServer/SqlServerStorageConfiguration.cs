68cf9b8a3d5a7b2b7b35c424f03900add378a34f
Michael Brown <mikeb@360replays.com>
2022-01-10 15:18:19 -0800

    Initial commit

---
+++ Binner.StorageProvider.SqlServer/Binner.StorageProvider.SqlServer/SqlServerStorageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Binner.StorageProvider.SqlServer
+{
+    public class SqlServerStorageConfiguration
+    {
+        public string ConnectionString { get; set; }
+
+        public SqlServerStorageConfiguration()
+        {
+        }
+
+        public SqlServerStorageConfiguration(IDictionary<string, string> config)
+        {
+            if (config.ContainsKey("ConnectionString"))
+                ConnectionString = config["ConnectionString"];
+        }
+    }
+}
